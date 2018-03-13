a8ce39c209453484315e374beef42da870495f1c
BA88 <benziane.abdelkader88@gmail.com>
2018-03-13 12:25:13 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-
+    <script type='text/javascript'>
+//<![CDATA[	
+    jQuery(document).ready(function() {
+    jQuery('.sidebar-wrapper').theiaStickySidebar({
+      // Settings
+            additionalMarginTop: 30,
+            additionalMarginBottom: 0,
+    });
+  });
+//]]>	
+    </script>
